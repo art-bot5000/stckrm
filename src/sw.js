13308d94b61dea12cdb9934877cfb5dd92868d36
art-bot5000 <pete@artbot5000.com>
2026-04-13 16:20:46 +0100

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 // ── INCREMENT THIS VERSION NUMBER EVERY TIME YOU DEPLOY ──────
-const CACHE_VERSION = 'stockroom-kv-v63';
+const CACHE_VERSION = 'stockroom-kv-v64';
 const CACHE_NAME    = CACHE_VERSION;
 
 const CACHE_URLS = [
