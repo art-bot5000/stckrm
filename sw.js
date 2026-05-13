9c0e5fa87fcc5ec0c0d0e96bbbf474691f3615ee
art-bot5000 <pete@artbot5000.com>
2026-05-13 12:58:45 +0100

    sw 336 grocery list fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'stockroom-kv-v335';
+const CACHE_VERSION = 'stockroom-kv-v336';
 // Namespace the cache by hostname so staging and production PWAs don't
 // fight over the same cache when both are installed on the same device.
 // Production hostnames (stckrm.com, app.stckrm.com, stckrm.fly.dev) all
